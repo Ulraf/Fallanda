ef053e3cbe4c65fdddcad9afae67f26b394d1c66
Shay Mualem <shaymu01@gmail.com>
2022-10-01 19:58:37 +0300

    merge

--- main.gd
+++ main.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 
 var score
 export var base_fall_speed = 500
@@ -8,10 +8,14 @@ var fall_velocity = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    score = 0
+	score = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    fall_speed += fall_velocity * delta
-    $HUD/Score.text = str(int(fall_speed))
+	fall_speed += fall_velocity * delta
+	$HUD/Score.text = str(int(fall_speed))
+
+func update_score(addToScore):
+	score += addToScore
+	$HUD/Score.text = str(int(score))
--- playerController.gd
+++ playerController.gd
@@ -9,30 +9,30 @@ var speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    playing = true
+	playing = true
 
 func _input(event):
-    if event is InputEventMouseButton:
-        if event.is_pressed():
-            pressed = true
-            prev_touch_pos = event.position
-        else:
-            pressed = false
-            direction = 0
-        
-    if pressed && event is InputEventMouseMotion:
-        var motion = event.position.distance_to(prev_touch_pos)
-        if (motion > sensativity):
-            if (event.position.direction_to(prev_touch_pos).x > 0):
-                direction = -1
-            else:
-                direction = 1
-        prev_touch_pos = event.position
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			pressed = true
+			prev_touch_pos = event.position
+		else:
+			pressed = false
+			direction = 0
+		
+	if pressed && event is InputEventMouseMotion:
+		var motion = event.position.distance_to(prev_touch_pos)
+		if (motion > sensativity):
+			if (event.position.direction_to(prev_touch_pos).x > 0):
+				direction = -1
+			else:
+				direction = 1
+		prev_touch_pos = event.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    if direction != 0:
-        var movement = delta * speed * direction;
-        position.x += movement
-    
-    speed_scale = get_node("/root/Main").fall_speed / get_node("/root/Main").base_fall_speed
+	if direction != 0:
+		var movement = delta * speed * direction;
+		position.x += movement
+	
+	speed_scale = get_node("/root/Main").fall_speed / get_node("/root/Main").base_fall_speed
