b56930ebbe86f6641d5e2e2c9cba2e0720138f64
Shay Mualem <shaymu01@gmail.com>
2022-10-01 18:33:31 +0300

    background music + bamboo collectable

---
+++ main.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var score
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	score = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	update_score(delta)
+
+func update_score(addToScore):
+	score += addToScore
+	$HUD/Score.text = str(int(score))
